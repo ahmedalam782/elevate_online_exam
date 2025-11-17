69270b9d394f5bb9adcb3c0338c8d6b37c4b079c
Mustafa Ghazi <ghazimostafa10@icloud.com>
2025-11-17 02:22:45 +0100

    feat(login): update remember-me behavior
    - Store token in secure storage regardless of 'remember me'
    - Save 'remember me' preference in shared preferences

--- lib/features/login/api/datasources/login_local_data_source_impl.dart
+++ lib/features/login/api/datasources/login_local_data_source_impl.dart
@@ -3,19 +3,33 @@ import 'package:elevate_online_exam/core/helper/session_token/session_token.dart
 import 'package:elevate_online_exam/features/login/data/datasources/login_local_data_source_contract.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:injectable/injectable.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 @LazySingleton(as: LoginLocalDataSourceContract)
 class LoginLocalDataSourceImpl implements LoginLocalDataSourceContract {
   FlutterSecureStorage secureStorage;
+  SharedPreferences sharedPreferences;
 
-  LoginLocalDataSourceImpl({required this.secureStorage});
+  LoginLocalDataSourceImpl({
+    required this.secureStorage,
+    required this.sharedPreferences,
+  });
   @override
-  Future<void> saveToken(String token) async {
+  Future<void> saveToken(String? token) async {
     try {
       await secureStorage.write(key: Apikeys.accessToken, value: token);
     } catch (e) {
       // if couldn't save the token in secure storage, save it in SessionToken Singleton for one time use
-      SessionToken().token = token;
+      secureStorage.write(key: Apikeys.accessToken, value: null);
+    }
+  }
+
+  @override
+  Future<void> saveRememberMe(bool isRemeberMe) async {
+    try {
+      await sharedPreferences.setBool(Apikeys.rememberMe, isRemeberMe);
+    } catch (e) {
+      sharedPreferences.setBool(Apikeys.rememberMe, false);
     }
   }
 }
