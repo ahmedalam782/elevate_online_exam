5e087001dec15c8380656817bd116732d34da6dc
ahmedalam <ahmedalam4887482@gmail.com>
2025-12-02 21:19:44 +0200

    refactor(app_layout): simplify AppLayoutPage structure and enhance loading indicator

--- lib/features/app_layout/presentation/view/pages/app_layout_page.dart
+++ lib/features/app_layout/presentation/view/pages/app_layout_page.dart
@@ -1,53 +1,59 @@
-
 import 'package:easy_localization/easy_localization.dart';
+import 'package:elevate_online_exam/core/errors/handle_errors/handle_errors.dart';
+import 'package:elevate_online_exam/core/shared/widgets/custom_toast.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lottie/lottie.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:toastification/toastification.dart';
 
 import '../../../../../core/config/base_state/base_state.dart';
 import '../../../../../core/config/di/injectable_config.dart';
-import '../../../../../core/errors/handle_errors/handle_errors.dart';
 import '../../../../../core/helper/user_helper/user_helper.dart';
 import '../../../../../core/languages/locale_keys.g.dart';
 import '../../../../../core/shared/widgets/custom_app_bar.dart';
-import '../../../../../core/shared/widgets/custom_toast.dart';
+import '../../../../../core/theme/app_animations.dart';
 import '../../../../../core/theme/app_colors.dart';
 import '../../view_model/cubit/app_layout_cubit.dart';
+import '../../view_model/cubit/app_layout_events.dart';
 import '../../view_model/cubit/app_layout_states.dart';
 import '../widgets/bottom_nav_bar.dart';
 
-class AppLayoutPage extends StatefulWidget {
-  const AppLayoutPage({super.key});
-
-  @override
-  State<AppLayoutPage> createState() => _AppLayoutPageState();
-}
-
-class _AppLayoutPageState extends State<AppLayoutPage> {
+class AppLayoutPage extends StatelessWidget {
+  AppLayoutPage({super.key});
   final AppLayoutCubit cubit = getIt<AppLayoutCubit>();
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => cubit,
       child: BlocConsumer<AppLayoutCubit, AppLayoutStates>(
         builder: (context, state) {
-          return Scaffold(
-            appBar: CustomAppBar(
-              title: cubit.titles[state.changeIndexState?.currentIndex ?? 0],
-              textColor: AppColors.primaryLight,
+          return ModalProgressHUD(
+            inAsyncCall: state.logoutState?.state == StateType.loading,
+            progressIndicator: Center(
+              child: Lottie.asset(
+                AppAnimations.animationsLoadingAnimationBlue,
+                width: 100,
+                height: 100,
+              ),
             ),
-            // body: cubit.pages[state.changeIndexState?.currentIndex ?? 0],
-            body: IndexedStack(
-              index: state.changeIndexState?.currentIndex ?? 0,
-              children: cubit.pages,
+            child: Scaffold(
+              appBar: CustomAppBar(
+                title: cubit.titles[state.changeIndexState?.currentIndex ?? 0],
+                textColor: AppColors.primaryLight,
+                actions: [
+                  IconButton(
+                    onPressed: () => cubit.doIndented(LogoutEvent()),
+                    icon: Icon(Icons.logout, color: AppColors.primaryLight),
+                  ),
+                ],
+              ),
+              body: IndexedStack(
+                index: state.changeIndexState?.currentIndex ?? 0,
+                children: cubit.pages,
+              ),
+              bottomNavigationBar: BottomNavBar(),
             ),
-            bottomNavigationBar: BottomNavBar(),
           );
         },
         listener: (context, state) {
